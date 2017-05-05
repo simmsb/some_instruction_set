f0ba5ba9f656daf500ffa84e4209b251cd70c5b2
nitros12 <bsimms.simms@gmail.com>
2017-05-05 22:30:21 +0100

    more work on this, idk

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -22,12 +22,84 @@ class Register(enum.Enum):
     acc = 10
 
 
-def match_enum(enum_):
+class Dereference:
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        """No."""
+        return f"[{self.value}]"
+
+    def expand(self):
+        yield from consume_math(*self.value)
+
+
+def match_enum(enum_, *, prefix=None):
     """Return a parser that matches an enum and has the parse action set to the Enum."""
-    match = reduce(operator.or_, (pp.Literal(i.name) for i in enum_))  # type: pp.MatchFirst
+    def get_tok(tok):
+        if prefix:
+            return pp.Combine(pp.Literal(prefix).suppress() + pp.Literal(tok))
+        return pp.Literal(tok)
+
+    match = reduce(operator.or_, (get_tok(i.name) for i in enum_))  # type: pp.MatchFirst
     catch = match.setParseAction(lambda t: enum_[t[0]])
     return catch
 
 
+class Operation:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.name}"
+
+
+class Push(Operation):
+    pass
+
+
+class Op(Operation):
+    pass
+
+
+class Deref:
+    pass
+
+
+def consume_math(*math):
+    """Consume some math expression."""
+    if len(math) == 1:
+        yield math[0]
+
+    def bin_op(left, op, right, *rest):
+        def expansion(val):
+            if isinstance(val, (Register, int)):
+                yield Push(val)
+            elif isinstance(val, Dereference):
+                yield from val.expand()
+                yield Deref()
+            elif isinstance(val, (list, pp.ParseResults)):
+                yield from consume_math(*val)
+
+        yield from expansion(left)
+        yield from expansion(right)
+        yield Op(op)
+
+        if rest:
+            yield from bin_op(None, *rest)
+
+    yield from bin_op(*math)
+
+
 class Parsing:
-    register = match_enum(Register)
+    register = match_enum(Register, prefix='%')
+    integer = pp.Word(pp.nums).setParseAction(lambda t: int(t[0]))
+
+    expr = pp.Forward()
+
+    combinator = register | integer | pp.nestedExpr(content=expr) | \
+        pp.nestedExpr(opener="[", closer="]", content=expr).setParseAction(lambda t: Dereference(t[0]))
+
+    expr <<= combinator + pp.ZeroOrMore(pp.oneOf("+ -") + combinator)
--- assembler/readme.md
+++ assembler/readme.md
@@ -14,4 +14,28 @@ Two Psh (Reg False 0)
 [Three Mov (Reg True 1) (Mem False 44),Three Mov (Reg True 1) (Mem True 22),One Nop,Two Psh (Reg False 0)]
 *Main> assemble n
 "8001c001002c8001c0018016000040014000"
-*Main>```
+*Main>
+```
+
+
+Memory location parser:
+
+Turns
+`%aaa + 3 - [3 + (4 - %bbb)]`
+
+Into
+```
+Push: Register.aaa
+Push: 3
+Op: +
+Push: 3
+Push: 4
+Push: Register.bbb
+Op: -
+Op: +
+Deref
+Op: -
+```
+
+Result would be left in a register somewhere, then use dereference of a register to get to there
+Will have to think of something for how to assign to registers, and some optimisation stuff for this
